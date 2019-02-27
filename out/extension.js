dbea46ec697ae949ee3936eda83e376d0d89f0c1
Johannes Drechsler <johannes.drechsler@arvatosystems.com>
2019-02-27 17:52:47 -0500

    Initial Commit

---
+++ out/extension.js
@@ -0,0 +1,26 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const vscode = require("vscode");
+function activate(context) {
+    context.subscriptions.push(vscode.commands.registerCommand('vueComponentPreview.start', () => {
+        // Create and show a new webview
+        const panel = vscode.window.createWebviewPanel('vueComponent', // Identifies the type of the webview. Used internally
+        'Vue Component Preview', // Title of the panel displayed to the user
+        vscode.ViewColumn.Beside, // Editor column to show the new webview panel in.
+        {
+            retainContextWhenHidden: true,
+            enableScripts: false
+        } // Webview options. More on these later.
+        );
+        panel.webview.html = getComponentHTML();
+        panel.onDidDispose(() => {
+            // When the panel is closed, cancel any future updates to the webview content
+            console.log('panel was closed');
+        }, null, context.subscriptions);
+    }));
+}
+exports.activate = activate;
+function getComponentHTML() {
+    return "";
+}
+//# sourceMappingURL=extension.js.map
