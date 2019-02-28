254bad3599e24aefb804bc4ebf3b495d0f51e6d0
JDrechsler <johannes.drechsler@arvatosystems.com>
2019-02-27 22:30:47 -0500

    Added html preview, template preview and doc events

--- src/extension.ts
+++ src/extension.ts
@@ -1,33 +1,70 @@
 import * as vscode from 'vscode';
 
+const regVueTemplate = new RegExp('(?<=<template>).*(?=</template>)', 's');
+const supportedDocLanguages = ['html', 'vue'];
+let currentFile = '';
+
 export function activate(context: vscode.ExtensionContext) {
-	context.subscriptions.push(
-		vscode.commands.registerCommand('vueComponentPreview.start', () => {
-			// Create and show a new webview
-			const panel = vscode.window.createWebviewPanel(
-				'vueComponent', // Identifies the type of the webview. Used internally
-				'Vue Component Preview', // Title of the panel displayed to the user
-				vscode.ViewColumn.Beside, // Editor column to show the new webview panel in.
-				{
-                    retainContextWhenHidden:true,
-                    enableScripts: false
-                } // Webview options. More on these later.
-            );
-
-            panel.webview.html = getComponentHTML();
-
-            panel.onDidDispose(
-				() => {
-					// When the panel is closed, cancel any future updates to the webview content
-                    console.log('panel was closed')
-				},
-				null,
-				context.subscriptions
-			);
-		})
-	);
+  context.subscriptions.push(
+    vscode.commands.registerCommand('vueComponentPreview.start', () => {
+      startExtension(context);
+    })
+  );
 }
 
-function getComponentHTML():string{
-    return "";
+function startExtension(context: vscode.ExtensionContext) {
+  const panel = vscode.window.createWebviewPanel(
+    'vueComponent',
+    'Vue Component Preview',
+    vscode.ViewColumn.Beside,
+    {
+      retainContextWhenHidden: true,
+      enableScripts: false
+    }
+  );
+  panel.onDidDispose(() => {}, null, context.subscriptions);
+  panel.webview.html = getComponentHTML();
+
+  vscode.window.onDidChangeTextEditorSelection(e => {
+    var fileSelected = e.textEditor.document.fileName;
+    if (fileSelected !== currentFile) {
+      updatePanel();
+      currentFile = fileSelected;
+    }
+  });
+
+  vscode.workspace.onDidChangeTextDocument(e => {
+    if (e.contentChanges.length > 0) {
+      if (docLangIsSupported) {
+        updatePanel();
+      }
+    }
+  });
+
+  function updatePanel() {
+    if (docLangIsSupported) {
+      panel.webview.html = getComponentHTML();
+    }
+  }
+}
+
+function docLangIsSupported(): boolean {
+  const currDocLang = vscode.window.activeTextEditor.document.languageId;
+  const docLangIsSupported = supportedDocLanguages.includes(currDocLang);
+  return docLangIsSupported;
+}
+
+function getComponentHTML(): string {
+  const currentDocText = vscode.window.activeTextEditor.document.getText();
+  const docLanguageId = vscode.window.activeTextEditor.document.languageId;
+
+  if (docLanguageId.toLocaleLowerCase() === 'html') {
+    return vscode.window.activeTextEditor.document.getText();
+  } else if (docLanguageId.toLocaleLowerCase() === 'vue') {
+    const vueTemplateMatches = regVueTemplate.exec(currentDocText);
+    if (vueTemplateMatches.length > 0) {
+      return vueTemplateMatches[0];
+    }
+  }
+  return currentDocText;
 }
